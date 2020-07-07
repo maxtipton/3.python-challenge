9224320c7f449978dba5e14f80b6b1fef62d6cd8
Paul Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-07-07 13:22:54 -0400

    Adding setup files per homework instructions

---
+++ PyBank/main.py
@@ -0,0 +1,36 @@
+
+
+
+# import/read the csv
+# 
+# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. 
+# You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses. 
+# (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+
+
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+#1. The total number of months included in the dataset
+#do this: 
+# creating lists/blank lists
+
+#2. The net total amount of "Profit/Losses" over the entire period
+#do this: 
+# Net Total = 0, adding to that variable as you go through.
+    
+#3. The average of the changes in "Profit/Losses" over the entire period
+#do these: 
+# use the append function as you loop through
+#adding values to lists; then how many values are in those lists?
+#storing changes #
+
+#4. The greatest increase in profits (date and amount) over the entire period
+#do this: 
+# max(the list in which i've stored all the month to month changes)
+
+#5. The greatest decrease in losses (date and amount) over the entire period
+#do this: 
+# min(the list in which i've stored all the month to month changes)
+
+
+
