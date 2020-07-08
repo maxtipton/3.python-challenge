129baa963e4ae15a01a470242e936daca4fea4d1
Paul Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-07-08 00:48:09 -0400

    Add existing file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,76 @@
+# In this challenge, you are tasked with creating 
+# a Python script for analyzing the financial records of your company. 
 
+#Calculate each of the following:
 
+# import modules
+import os
+import csv
 
-# import/read the csv
-# 
-# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. 
-# You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses. 
-# (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+#declare CSV path 
+csvpath = os.path.join("/Users/bartholdy9000/Desktop/UPenn Bootcamp/Homework/3. python-challenge/PyBank/Resources/budget_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
 
+   # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    data = []
 
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
+    for row in csvreader:
+        d={}
+        d['month'] = row[0]
+        d['net'] = int(row[1])
+        data.append(d)
 
 #1. The total number of months included in the dataset
-#do this: 
-# creating lists/blank lists
+months = []
+for d in data: 
+    months.append(d['month'])
+    
+total_months = len(months)
+print("Total Months: ", total_months),
 
 #2. The net total amount of "Profit/Losses" over the entire period
-#do this: 
-# Net Total = 0, adding to that variable as you go through.
+net = []
+for d in data: 
+    net.append(d['net'])
     
+total_net = sum(net)
+print("Total: $", total_net)
+
 #3. The average of the changes in "Profit/Losses" over the entire period
-#do these: 
-# use the append function as you loop through
-#adding values to lists; then how many values are in those lists?
-#storing changes #
+diffs = []
+greatest_net_sofar = 0
+least_net_sofar = 0
+greatest_month_sofar = ""
+least_month_sofar = ""
+
+for index,d in enumerate(data[1:]): 
+    previous_net = data[index - 1]['net']
+    diff = d['net'] - previous_net
+    diffs.append(diff)
+    if diff > greatest_net_sofar:
+        greatest_net_sofar = diff
+        greatest_month_sofar = d["month"]
+    if diff < least_net_sofar:
+        least_net_sofar = diff
+        least_month_sofar = d["month"]
+
+average = sum(diffs) / len(diffs) 
+print("Average Change: $", average)
 
 #4. The greatest increase in profits (date and amount) over the entire period
-#do this: 
-# max(the list in which i've stored all the month to month changes)
+print("Greatest Increase in Profits: :", greatest_month_sofar, greatest_net_sofar)
 
 #5. The greatest decrease in losses (date and amount) over the entire period
-#do this: 
-# min(the list in which i've stored all the month to month changes)
+print("Greatest Decrease in Profits: :", least_month_sofar, least_net_sofar)
 
-
+with open('PyBank.txt', 'w') as text_file:
+    print(f'Financial Analysis', file=text_file)
+    print(f'___________________________', file=text_file)
+    print(f'Total Months: {total_months}', file=text_file)
+    print(f'Total: ${total_net}', file=text_file)
+    print(f'Average Change: ${average}', file=text_file)
+    print(f'Greatest Increase in Profits: {greatest_month_sofar} ({greatest_net_sofar})', file=text_file)
+    print(f'Greatest Decrease in Profits: {least_month_sofar} ({least_net_sofar})', file=text_file)
 
